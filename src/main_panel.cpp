4ee74058196debd15652df02f4d0d53174ab278e
Jason Pell <jason@pellcorp.com>
2026-08-30 17:26:46 +1000

    I actually broke led button entirely with the change, cant rely on the virtual printer for state

--- src/main_panel.cpp
+++ src/main_panel.cpp
@@ -305,9 +305,9 @@ void MainPanel::create_fans(json &fans) {
 void MainPanel::create_leds(json &leds) {
   std::lock_guard<std::mutex> lock(lv_lock);
   if (leds.is_array() && !leds.empty()) {
-    led_btn.show();
+    led_btn.enable();
   } else {
-    led_btn.hide();
+    led_btn.disable();
   }
   led_panel.init(leds);
   led_btn.set_image(led_panel.get_main_button_image());
